e88a4c845ad6e7d6118974b566dafd659943b63a
Nikita Ugnich <nikitagavrilin005@gmail.com>
2025-10-13 07:14:41 +0300

    uhm

--- main.py
+++ main.py
@@ -4,10 +4,18 @@ from fastapi import FastAPI, Request
 from fastapi.responses import FileResponse, HTMLResponse, Response
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware
 from datetime import datetime
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.mount('/static', StaticFiles(directory='static'), name='static')
 
